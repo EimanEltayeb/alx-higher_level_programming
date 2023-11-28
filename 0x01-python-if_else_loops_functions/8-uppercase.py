c62987717298d8746de62e7251162e89344be1fa
EimanEtltayeb <em_moh092@hotmail.com>
2023-11-28 11:02:30 -0800

    grdsg

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        if ord(i) > 96 and ord(i) < 123:
+            print("{0}".format(char(ord(i) + 32)), end="")
+        else:
+            print(("{0}".format(char(ord(i))), end="")
