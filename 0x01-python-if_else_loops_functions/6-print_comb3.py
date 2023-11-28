96586ba7a8102f7792ad596f09147dfc0277ff98
EimanEtltayeb <em_moh092@hotmail.com>
2023-11-28 09:47:44 -0800

    s

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+for i in range(0, 8):
+    for j in range(0, 10):
+        if i < j:
+            print("{0}{1}".format(i, j), end=", ")
+        else:
+            continue
+print("{0}{1}".format(i + 1, j))
