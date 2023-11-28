9e01d583f9221251e5cc20aaf19d7e5f915fbdbf
EimanEtltayeb <em_moh092@hotmail.com>
2023-11-27 16:15:05 -0800

    d

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,5 +1,2 @@
 #!/usr/bin/python3
-a = " is better than "
-b = "If the implementation is "
-c = " to explain, "
-print(f"The Zen of Python, by Tim Peters\n\nBeautiful{a}ugly.\nExplicit{a}implicit.\nSimple{a}complex.\nComplex{a}complicated.\nFlat{a}nested.\nSparse{a}dense.\nReadability counts.\nSpecial cases aren't special enough to break the rules.\nAlthough practicality beats purity.\nErrors should never pass silently.\nUnless explicitly silenced.\nIn the face of ambiguity, refuse the temptation to guess.\nThere should be one-- and preferably only one --obvious way to do it.\nAlthough that way may not be obvious at first unless you're Dutch.\nNow is better than never.\nAlthough never is often better than *right* now.\n{b}hard{c}it's a bad idea.\n{b}easy{a}it may be a good idea.\nNamespaces are one honking great idea -- let's do more of those!")
+import this
