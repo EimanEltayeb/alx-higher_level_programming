f256fa95e5756af5ebfa79dbe94c285b3c9b9ad1
EimanEtltayeb <em_moh092@hotmail.com>
2023-11-28 13:05:40 -0800

    gfdg

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -4,9 +4,9 @@ def pow(a, b):
     if b >= 0:
         for i in range(0, b):
             x = x * a
+        return (x)
     else:
         b2 = -1 * b
         for i in range(0, b2)
             x = x * a
-        x = 1 / x
-    return (x)
+    return (1 / x)
