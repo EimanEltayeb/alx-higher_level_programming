2d9143568123b7ee2b3d28a609c4aa5a532c7ece
EimanEtltayeb <em_moh092@hotmail.com>
2023-11-29 14:00:01 -0800

    gdv

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ *
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *temp1;
+	listint_t *temp2;
+
+	temp1 = list;
+	temp2 = list->next;
+
+	while (temp1 != NULL && temp2 != NULL)
+	{
+		if (temp1 == temp2)
+			return (1);
+		else
+		{
+			temp1 = temp1->next;
+			temp2 = temp2->next->next;
+		}
+	}
+	return (0);
+}
