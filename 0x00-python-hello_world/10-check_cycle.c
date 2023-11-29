046cb47bbfa064beae5d5b2deb77f33b7a7a7381
EimanEtltayeb <em_moh092@hotmail.com>
2023-11-29 15:41:04 -0800

    fdv

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,10 +7,10 @@ int check_cycle(listint_t *list)
 	listint_t *temp1;
 	listint_t *temp2;
 
-	temp1 = list;
-	temp2 = list->next;
 	if (list == NULL)
 		return (0);
+	temp1 = list;
+	temp2 = list->next;
 
 	while (temp1 != NULL && temp2->next != NULL)
 	{
