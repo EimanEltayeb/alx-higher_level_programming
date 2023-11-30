d8e1e26bd1da04f02a8bea021a4b73d55ed31484
EimanEtltayeb <em_moh092@hotmail.com>
2023-11-30 12:11:10 -0800

    gdfb

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-
+if __name__ == '__main__':
+    from sys import argv
+    lenn = len(argv)
+    total = 0
+    for i in range(1, lenn):
+        total += int(argv[i])
+    print(total)
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
+    hidden_4.python3.8.x
+
     print("my_secret_santa")
     print("print_school")
     print("print_hidden")
