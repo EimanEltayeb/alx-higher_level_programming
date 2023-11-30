137d93521c7707421914ccd4c1cea99976ff854a
EimanEtltayeb <em_moh092@hotmail.com>
2023-11-30 12:42:50 -0800

    gdsgv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
-
-
+if __name__ == '__main__':
+    lenn = len(argv) - 1
+    if lenn == 0:
+        print("{0} arguments.".format(lenn))
+    elif lenn == 1:
+        print("{0} argument:".format(lenn))
+    else:
+        print("{0} arguments:".format(lenn))
+    if lenn > 0:
+        for i in range(1, len(argv)):
+            print("{0}: {1}".format(i, argv[i]))
