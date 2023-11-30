b6f5fecc7234978e42e57fe904538ad1d1f2d8c9
EimanEtltayeb <em_moh092@hotmail.com>
2023-11-29 16:07:41 -0800

    gdfghb

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for n in range(122:95)
+for n in range(122,95,-1):
     if n % 2 == 0:
         char = chr(n)
     else:
         char = chr(n - 32)
-    print("{}".format(char), end="")
+    print("{0}".format(char), end="")
 
