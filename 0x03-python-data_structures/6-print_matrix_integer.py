7dbf580f8ab3b6eb7ebcee06610ab3ffeefd1bd4
EimanEtltayeb <em_moh092@hotmail.com>
2023-12-03 13:20:37 -0800

    ghfhg

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix == None or len(matrix) == 0:
-        print("\n")
+    if len(matrix[0]) == 0:
+        print("")
     else:
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
