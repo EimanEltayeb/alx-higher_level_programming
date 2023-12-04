84d6da210ed25f77ef5e44f78083c889e3750d3e
EimanEtltayeb <em_moh092@hotmail.com>
2023-12-04 11:59:20 -0800

    gd

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    a = sentence[0]
     x = len(sentence)
-    t = (x, a)
+    if x == 0:
+        a = None
+    else:
+        a = sentence[0]
     return (x, a)
