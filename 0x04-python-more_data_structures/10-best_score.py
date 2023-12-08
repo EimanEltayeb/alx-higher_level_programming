4313e2b2427075e16f376f27eef7f4d6c9c6f25a
EimanEltayeb <em_moh092@hotmail.com>
2023-12-08 13:43:35 -0800

    jghk

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    best = 0
+    i = None
+    if not a_dictionary:
+        return None
+    for x in a_dictionary:
+        if a_dictionary[x] > best:
+            i = x
+            best = a_dictionary[x]
+    return i
