440ee30bc1c9d8014c0409d47abccd5e321a1c1d
EimanEltayeb <em_moh092@hotmail.com>
2024-01-04 01:14:23 -0800

    d

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -56,6 +56,8 @@ class Square:
         if self.__size == 0:
             print("")
         else:
+            for x in range(self.__position[1]):
+                print("")
             for i in range(self.__size):
                 for p in range(self.__position[0]):
                     print(" ", end="")
