85356490fc9df778434ad07ac93ce37a259da6d2
EimanEltayeb <em_moh092@hotmail.com>
2024-01-08 00:45:55 -0800

    d

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" prints a text with 2 new lines after certain characters"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for i in range(len(text)):
+        if text[i] in ('.', '?', ':'):
+            print(text[i])
+            print("")
+        else:
+            if text[i - 1] in ('.', '?', ':') and text[i] == ' ':
+                continue
+            else:
+                print(text[i], end="")
