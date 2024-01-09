d47ef0ea420078ad2c4247d1434729f65f2ef550
EimanEltayeb <em_moh092@hotmail.com>
2024-01-09 13:11:29 -0800

    d

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" writes a string to a text file and return charachter number"""
+
+
+def write_file(filename="", text=""):
+    """ writes a string to a text file and return charachter number"""
+
+    with open(filename, "w") as file:
+        file.write(text)
+        return len(text)
