481ced1a18f46de3597d287dd63e833c14a585de
EimanEltayeb <em_moh092@hotmail.com>
2024-01-11 02:17:27 +0300

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""this module contains function print_sorted"""
+
+
+class MyList(list):
+    """class MyList"""
+
+    def __init__(self, my_list=[]):
+        """initialization"""
+        self.my_list = my_list
+
+    def print_sorted(self):
+        """method to print sorted list"""
+        x = sorted(self)
+        print(x)
