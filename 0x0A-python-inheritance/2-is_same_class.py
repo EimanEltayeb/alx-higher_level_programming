57e4adbd0171a02e318bfb4dea45883c48ce0c64
EimanEltayeb <em_moh092@hotmail.com>
2024-01-11 02:35:24 +0300

    2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""This module contains iis_same_class function"""
+
+
+def is_same_class(obj, a_class):
+    """ This function compares the types of the inputs"""
+
+    return type(obj) is a_class
