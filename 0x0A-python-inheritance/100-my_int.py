c265eb827057f1beca13a1ff0c4784234c8ae18d
EimanEltayeb <em_moh092@hotmail.com>
2024-01-11 07:30:59 -0800

    100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""the module contains the class MyInt"""
+
+
+class MyInt(int):
+    """My int class"""
+
+    def __init__(self, nt):
+        """init"""
+
+        self.nt = nt
+        super().__init__()
+
+    def __eq__(self, other):
+        """str"""
+
+        if self.nt == other.nt:
+            return False
+        return True
+
+    def __ne__(self, other):
+        """not equal"""
+
+        if self.nt != other.nt:
+            return True
+        return False
+
+
