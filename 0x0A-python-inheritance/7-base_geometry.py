5b16c59409d8856b0c410f3ba6e6c589234972c0
EimanEltayeb <em_moh092@hotmail.com>
2024-01-11 03:14:34 +0300

    d

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""module that creat empty class"""
+
+
+class BaseGeometry:
+    """ BaseGeometry class"""
+
+    def __init__(self):
+        """initialising the class"""
+
+        pass
+
+    def area(self):
+        """area method"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """integer validation method"""
+
+        if not isinstance(value, int):
+            raise TypeError(f"{name} be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
