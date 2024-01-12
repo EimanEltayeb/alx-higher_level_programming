0de445f6fd06d0c1d421138fd19a011568f72c21
EimanEltayeb <em_moh092@hotmail.com>
2024-01-12 15:46:21 -0800

    pycodestyle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,22 +6,21 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-	"""Square Class"""
+    """Square Class"""
 
-	def __init__(self, size, x=0, y=0, id=None):
-		self.validate_h_w("width", size)
-		super().__init__(size, size, x, y, id)
-		self.__size = size
+    def __init__(self, size, x=0, y=0, id=None):
+        self.validate_h_w("width", size)
+	super().__init__(size, size, x, y, id)
+	self.__size = size
 
-	
-	@property
-	def size(self):
-		return self.__size
-	
-	@size.setter
-	def size(self, value):
-		self.validate_h_w("width", value)
-		self.__size = value
+    @property
+    def size(self):
+	return self.__size
 
-	def __str__(self):
-		return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+    @size.setter
+    def size(self, value):
+	self.validate_h_w("width", value)
+	self.__size = value
+
+    def __str__(self):
+	return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
