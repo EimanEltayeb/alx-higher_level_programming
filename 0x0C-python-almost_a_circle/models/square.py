929be5c878602c6d430bd7f2abbeeee4ee4a46b7
EimanEltayeb <em_moh092@hotmail.com>
2024-01-12 15:49:36 -0800

    d

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,8 +10,8 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         self.validate_h_w("width", size)
-	super().__init__(size, size, x, y, id)
-	self.__size = size
+        super().__init__(size, size, x, y, id)
+        self.__size = size
 
     @property
     def size(self):
@@ -19,8 +19,8 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-	self.validate_h_w("width", value)
+        self.validate_h_w("width", value)
 	self.__size = value
 
     def __str__(self):
-	return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
