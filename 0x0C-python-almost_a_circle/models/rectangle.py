1c60dc3fd1a8e04101ea07098cd440b70b89e2e6
EimanEltayeb <em_moh092@hotmail.com>
2024-01-13 01:01:09 +0300

    6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,6 +11,11 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         """init"""
 
+        self.validate_h_w("width", width)
+        self.validate_h_w("height", height)
+        self.validate_x_y("x", x)
+        self.validate_x_y("y", y)
+
         super().__init__(id)
         self.__width = width
         self.__height = height
@@ -23,6 +28,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        self.validate_h_w("width", value)
         self.__width = value
 
     @property
@@ -31,6 +37,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        self.validate_h_w("height", value)
         self.__height = value
 
     @property
@@ -39,6 +46,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        self.validate_x_y("x", value)
         self.__x = value
 
     @property
@@ -47,10 +55,29 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        self.validate_x_y("y", value)
         self.__y = value
 
-    def validate_h_w(name, value):
+    def validate_h_w(self, name, value):
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be > 0")
+
+    def validate_x_y(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value < 0:
+            raise ValueError(f"{name} must be >= 0")
+
+    def area(self):
+        return self.__height * self.__width
+    
+    def display(self):
+        for i in range(self.__height):
+            for i in range(self.__width):
+                print('#', end="")
+            print("")
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
