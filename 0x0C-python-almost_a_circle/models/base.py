305fa6d58ca991b0b8e68b17d5d97b4cbc1fa457
EimanEltayeb <em_moh092@hotmail.com>
2024-01-15 12:27:43 -0800

    s

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,12 +21,16 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """ turn the dictionary to json string"""
+
         if not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """save list of object to file"""
+
         dic_list = []
         for ob in list_objs:
             dic_list.append(ob.to_dictionary())
@@ -35,13 +39,31 @@ class Base:
 
     @staticmethod
     def from_json_string(json_string):
+        """convert the json string"""
+
         if not json_string:
             return []
         return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
+        """creats an object"""
+
         x = cls(1, 1)
         x.update(**dictionary)
         return x
 
+    @classmethod
+    def load_from_file(cls):
+        """load from file"""
+
+        try:
+            with open(f"{cls.__name__}.json", "r") as f:
+                list_d = cls.from_json_string(f.read())
+                ins_list = []
+                for d in list_d:
+                    x = cls.create()
+                    ins_list.append(x)
+                return ins_list
+        except FileNotFoundError:
+            return []
