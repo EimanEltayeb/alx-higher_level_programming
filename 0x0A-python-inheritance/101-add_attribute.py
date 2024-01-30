fd92bb9e8a1ff34e8f916e6779922b9206d7d7e3
EimanEltayeb <em_moh092@hotmail.com>
2024-01-29 19:34:04 -0800

    grgfb

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,47 +1,9 @@
 #!/usr/bin/python3
-""" module"""
+""" module 101-add_attribute.py """
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def add_attribute(clas, att, value):
+    if clas.att is None:
+        clas.att = value
+    elif clas.att is not None and clas.att != value:
+        raise TypeError("can't add new attribute")
