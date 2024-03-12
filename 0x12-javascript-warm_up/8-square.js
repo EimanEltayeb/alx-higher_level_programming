ebb5381b034c36b1de89437009606e530ae98bc3
EimanEltayeb <em_moh092@hotmail.com>
2024-03-12 05:02:50 -0700

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Missing size');
+} else {
+  const x = parseInt(process.argv[2]);
+  let st = '';
+  for (let i = 0; i < x; i++) {
+    st += 'X';
+  }
+  for (let i = 0; i < x; i++) {
+    console.log(st);
+  }
+}
