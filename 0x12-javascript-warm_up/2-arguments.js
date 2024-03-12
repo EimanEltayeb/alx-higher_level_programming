2d691862b05432a59983a4c454eb457a3ece045a
EimanEltayeb <140092109+EimanEltayeb@users.noreply.github.com>
2024-03-12 05:58:32 +0300

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,5 +5,11 @@ if (process.argv.length < 3)
 }
 else
 {
-  console.log("Argument found")
+  if (process.argv.length = 3)
+  {
+    console.log("Argument found")
+  }
+  else
+  {
+    console.log("Arguments found")
 }
