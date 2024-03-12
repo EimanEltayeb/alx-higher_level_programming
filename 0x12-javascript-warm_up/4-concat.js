2dd073e499be2076f3928ce69f1a9b8e1d7e41b2
EimanEltayeb <140092109+EimanEltayeb@users.noreply.github.com>
2024-03-12 06:09:21 +0300

    Update 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-let (s1 = process.argv[2], s2 = process.argv[3])
-  console.log(s1 + " is " + s2);
+const s1 = process.argv[2];
+const s2 = process.argv[3]);
+console.log(s1 + " is " + s2);
