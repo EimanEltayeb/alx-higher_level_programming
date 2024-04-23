21030845e6a7a3c97e0f0e3622ce24c395c1e953
EimanEltayeb <em_moh092@hotmail.com>
2024-04-23 10:16:24 -0700

    fdgv

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 SELECT tv_genres.name AS genre, COUNT(tv_shows.id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres
-	ON tv_genres.id =_show_genres.genre_id
+	ON tv_genres.id = show_genres.genre_id
 JOIN tv_shows
 	ON tv_show_genres.show_id = tv_shows.id
 ORDER BY number_of_shows DESC;
