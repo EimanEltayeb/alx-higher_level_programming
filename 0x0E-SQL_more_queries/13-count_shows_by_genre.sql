26ed8ca71f84444fb2abc2d4e7747950100e455b
EimanEltayeb <em_moh092@hotmail.com>
2024-04-23 10:14:24 -0700

    ngfn

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 SELECT tv_genres.name AS genre, COUNT(tv_shows.id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres
-	ON tv_show_genres.genre_id = tv_genres.id
+	ON tv_genres.id =_show_genres.genre_id
 JOIN tv_shows
-	ON tv_shows.id = tv_show_genres.show_id
+	ON tv_show_genres.show_id = tv_shows.id
 ORDER BY number_of_shows DESC;
