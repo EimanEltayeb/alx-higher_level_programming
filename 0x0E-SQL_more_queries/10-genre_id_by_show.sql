b38bda9a8730dc59ae54e82486a375d261cd672b
EimanEltayeb <em_moh092@hotmail.com>
2024-04-23 09:26:37 -0700

    gdfg

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,8 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows 
 JOIN tv_show_genres
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id  ASC;
+	ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres
+	ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
 
