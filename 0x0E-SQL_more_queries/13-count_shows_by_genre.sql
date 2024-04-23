ac6a941ab7dad0d83bdb795e98becb0b85818887
EimanEltayeb <em_moh092@hotmail.com>
2024-04-23 10:20:18 -0700

    fgd

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,4 +5,5 @@ JOIN tv_show_genres
 	ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows
 	ON tv_show_genres.show_id = tv_shows.id
+GROUP BY tv_genres.id, tv_genres.name
 ORDER BY number_of_shows DESC;
