62b2ab0979b99f8defed84a562f24ef301834b48
EimanEltayeb <em_moh092@hotmail.com>
2024-04-23 09:33:52 -0700

    gfgd

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- genre  id all shows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+	ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres
+	ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
