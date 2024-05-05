a1f0a3946d88a71a16d01f6c82055167f10d5bea
EimanEltayeb <em_moh092@hotmail.com>
2024-05-04 22:01:54 -0700

    g

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""2-my_filter_states.py"""
+
+
+import MySQLdb
+import sys
+
+
+def states_list(u, p, d, n):
+    """states_list"""
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=u, passwd=p, db=d)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        id_ = row[0]
+        name = row[1]
+        if name == n:
+        print(f"({id_}, '{name}')")
+
+    db.close()
+
+
+if __name__ == "__main__":
+    u = sys.argv[1]
+    p = sys.argv[2]
+    d = sys.argv[3]
+    n = sys.argv[4]
+
+    states_list(u, p, d, n)
